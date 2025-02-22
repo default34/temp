3899c1cf6ecb2ea1e39e13ed8172e8e65b686bb2
Artem Kozyrev <a.kozyrev@escape-team.tech>
2025-02-23 01:55:55 +0300

    add preview SN data

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,6 +1,9 @@
-// pages/index.js
+import Head from "next/head";
+
 export async function getServerSideProps() {
-  const res = await fetch("https://bobo-server-ssr-580f817d937d.herokuapp.com/api/users");
+  const res = await fetch(
+    "https://bobo-server-ssr-580f817d937d.herokuapp.com/api/users"
+  );
   const users = await res.json();
 
   return { props: { users } };
@@ -8,14 +11,32 @@ export async function getServerSideProps() {
 
 export default function Home({ users }: { users: any }) {
   return (
-    <div className='h-full w-full'>
-      <h1>Server-Side Rendered Page</h1>
-      {users.map((user: any) => {
-        return <div className='pb-[24px]'>
-          <div>{user.name}</div>
-          <div>{user.email}</div>
-        </div>
-      })}
-    </div>
+    <>
+      <Head>
+        <title>SSR Users</title>
+        <meta property="og:title" content="Server-Side Rendered Page" />
+        <meta
+          property="og:description"
+          content="It's alive!!!"
+        />
+        <meta
+          property="og:image"
+          content="https://public.bnbstatic.com/static/images/common/ogImage.jpg"
+        />
+        <meta property="og:url" content="https://temp-iota-ochre.vercel.app/" />
+        <meta name="twitter:card" content="summary_large_image" />
+      </Head>
+      <div className="h-full w-full">
+        <h1>Server-Side Rendered Page</h1>
+        {users.map((user: any) => {
+          return (
+            <div className="pb-[24px]">
+              <div>{user.name}</div>
+              <div>{user.email}</div>
+            </div>
+          );
+        })}
+      </div>
+    </>
   );
 }
